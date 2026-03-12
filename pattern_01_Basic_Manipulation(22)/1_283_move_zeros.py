8ca9a7172e05452aa7fd6c8aac9272fb9eb0355e
VN45 <vishalnagashankar4@gmail.com>
2026-03-12 22:38:41 +0530

    move zeros in place appraoch

--- pattern_01_Basic_Manipulation(22)/1_283_move_zeros.py
+++ pattern_01_Basic_Manipulation(22)/1_283_move_zeros.py
@@ -29,8 +29,18 @@ def inplace(nums):
                  j += 1
         return nums
 
+#here we are using unecessary swaps 
+def inplace_without_swap(nums):
+     j = 0
+     for i in range(len(nums)):
+          if nums[i] != 0:
+               nums[j] = nums[i]
+               j += 1
+     for k in range(j, len(nums)):
+          nums[k] = 0
+     return print(nums)
 
 
 
 nums = [0, 1, 0, 3, 12]
-print(inplace(nums))
+inplace_without_swap(nums)
